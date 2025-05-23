704beb4d28e9b77a976308ea1ea520482408cbfb
oguilherm3 <guilhermesa151@gmail.com>
2025-05-22 22:10:23 -0300

    feat: Implementação da Missoes Service com dependecia na Repository

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,22 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.Controller.NinjaModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    MissoesService(MissoesRepository missoesRepository){
+        this.missoesRepository = missoesRepository;
+    }
+
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+
 }
